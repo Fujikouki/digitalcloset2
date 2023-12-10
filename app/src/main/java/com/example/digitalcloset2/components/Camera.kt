a7e0d3f0dd929f1a67cfc810bd6fd1b034a5801b
Fujikouki <c1202599@st.kanazawa-it.ac.jp>
2023-12-10 16:38:53 +0900

    カメラ撮影機能の関数化

--- app/src/main/java/com/example/digitalcloset2/components/Camera.kt
+++ app/src/main/java/com/example/digitalcloset2/components/Camera.kt
@@ -1,11 +1,9 @@
 package com.example.digitalcloset2.components
 
 import android.content.ContentValues
+import android.content.Context
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Build
-import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
@@ -15,7 +13,6 @@ import androidx.camera.core.CameraSelector
 
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.ImageProxy
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 
@@ -43,13 +40,13 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
 import com.example.digitalcloset2.mainviewmodel
 
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionState
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
-import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -89,7 +86,6 @@ fun NoPermission(onRequestPermission: () -> Unit){
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CameraStert(Mainviewmodel: mainviewmodel){
-    val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
     val cameraController = remember {
@@ -105,61 +101,13 @@ fun CameraStert(Mainviewmodel: mainviewmodel){
                 },
                 icon = { Icon(imageVector = Icons.Default.Create, contentDescription = "写真を取る") },
                 onClick = {
-                    val mainExecutorService = ContextCompat.getMainExecutor(context)
-                    /*cameraController.takePicture(mainExecutorService, object : ImageCapture.OnImageCapturedCallback(){
-                        override fun onCaptureSuccess(image: ImageProxy) {
-                            val resolver = context.contentResolver
-                            val currentTimeMillis = System.currentTimeMillis()
-                            val displayName = "photo_$currentTimeMillis.jpg"
-                            val contentValues = ContentValues().apply {
-                                put(MediaStore.Images.Media.DISPLAY_NAME, displayName)
-                                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
-                                put(MediaStore.Images.Media.DATE_ADDED, currentTimeMillis / 1000) // 秒単位でのタイムスタンプ
-                                put(MediaStore.Images.Media.DATE_TAKEN, currentTimeMillis)
-                                // 必要に応じて、他のメタデータを追加できます
-                            }
-                            val imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-                            Log.d("URL",imageUri.toString())
-                            val buffer = image.planes[0].buffer
-                            val bytes = ByteArray(buffer.remaining())
-                            buffer.get(bytes)
-                            val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-                            Log.d("image34",image.toString())
-                            imageUri?.let { uri ->
-                                resolver.openOutputStream(uri)?.use { outputStream ->
-                                    // bitmapはあなたが保存したい画像です
-                                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-                                }
-                            }
-                            Mainviewmodel.ClothesImage = imageUri.toString()
-                            Log.d("カメラURL",Mainviewmodel.ClothesImage)
-                            image.close()
-                        }
-                    })*/
-                    val name = SimpleDateFormat(FILENAME_FORMAT, Locale.JAPAN)
-                        .format(System.currentTimeMillis())
-                    val contentValues = ContentValues().apply {
-                        put(MediaStore.MediaColumns.DISPLAY_NAME, name)
-                        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-                        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-                            put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
-                        }
-                    }
-                    val outputFileOptions = ImageCapture.OutputFileOptions
-                        .Builder(context.contentResolver,MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues).build()
-                    cameraController.takePicture(outputFileOptions,mainExecutorService,object :ImageCapture.OnImageSavedCallback{
-                        override fun onError(exception: ImageCaptureException) {
-                            Toast.makeText(context,exception.toString(),Toast.LENGTH_SHORT).show()
-                        }
-                        override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                            val savedUri = outputFileResults.savedUri
-                            savedUri?.let { uri ->
-                                Toast.makeText(context, "Image saved: $savedUri", Toast.LENGTH_SHORT).show()
-                                Mainviewmodel.ClothesImage = uri.toString()
-                            }
-                        }
-                    })
-                })
+                    tackCatcher(
+                        Mainviewmodel = Mainviewmodel,
+                        context = context,
+                        cameraController = cameraController
+                    )
+                }
+            )
         }
     ) { paddingValues:PaddingValues ->
         AndroidView(
@@ -177,16 +125,30 @@ fun CameraStert(Mainviewmodel: mainviewmodel){
     }
 }
 
-fun saveIamge(){
-
-    val  imageCapture = ImageCapture.Builder()
-        .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-        .build()
-
-}
-
-@Preview
-@Composable
-fun test(){
-    NoPermission(onRequestPermission = {})
+fun tackCatcher(Mainviewmodel: mainviewmodel,context: Context,cameraController: LifecycleCameraController){
+    val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    val mainExecutorService = ContextCompat.getMainExecutor(context)
+    val name = SimpleDateFormat(FILENAME_FORMAT, Locale.JAPAN)
+        .format(System.currentTimeMillis())
+    val contentValues = ContentValues().apply {
+        put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+            put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+        }
+    }
+    val outputFileOptions = ImageCapture.OutputFileOptions
+        .Builder(context.contentResolver,MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues).build()
+    cameraController.takePicture(outputFileOptions,mainExecutorService,object :ImageCapture.OnImageSavedCallback{
+        override fun onError(exception: ImageCaptureException) {
+            Toast.makeText(context,exception.toString(),Toast.LENGTH_SHORT).show()
+        }
+        override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+            val savedUri = outputFileResults.savedUri
+            savedUri?.let { uri ->
+                Toast.makeText(context, "Image saved: $savedUri", Toast.LENGTH_SHORT).show()
+                Mainviewmodel.ClothesImage = uri.toString()
+            }
+        }
+    })
 }
