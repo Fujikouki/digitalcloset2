032d88dbf4d9738a7df7f728067b719ff9758ec8
Fujikouki <c1202599@st.kanazawa-it.ac.jp>
2024-01-13 16:13:34 +0900

    UIに変更を加えた

--- app/src/main/java/com/example/digitalcloset2/components/ClothCaer.kt
+++ app/src/main/java/com/example/digitalcloset2/components/ClothCaer.kt
@@ -1,15 +1,16 @@
 package com.example.digitalcloset2.components
 
 import android.net.Uri
-import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
@@ -17,15 +18,20 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.example.digitalcloset2.ClothesData
+import com.example.digitalcloset2.R
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
@@ -42,34 +48,32 @@ fun ClothsRow(
             .clickable { onClickRow(cloth) }
             .padding(5.dp),
         elevation = CardDefaults.cardElevation(5.dp),
+        colors = CardDefaults.cardColors(containerColor = colorResource(id = R.color.closet),)
     ) {
-        Column(modifier = Modifier) {
-            Row(modifier = Modifier
-                .padding(5.dp),
-                verticalAlignment = Alignment.CenterVertically)
-            {
-                Column(modifier = Modifier.padding(5.dp)) {
-                    Text(text = cloth.ClothesName)
-                    Text(text = cloth.ClothesColor)
-                }
-                Spacer(modifier = Modifier.weight(1f))
-                Column(modifier = Modifier.padding(5.dp)) {
-                    Text(text = cloth.ClothesScene)
-                    Text(text = cloth.ClothesType)
-                }
-                Spacer(modifier = Modifier.weight(2f))
-                Column(modifier = Modifier.padding(5.dp)) {
-                    IconButton(
-                        onClick = { onClickDelete(cloth)
-                            Log.d("1234567",cloth.toString())
-                        })
-                    {
-                        Icon(imageVector = Icons.Default.Delete, contentDescription = "DeleteButton")
-                    }
-                }
-            }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .background(color = colorResource(id = R.color.teal_700)),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier.padding(5.dp),
+                text = cloth.ClothesName,
+                fontSize = 20.sp)
+        }
+        Row(modifier = Modifier) {
             GlideImage(model = Uri.parse(cloth.ClothesImage), contentDescription = "服の写真")
-            Log.d("cloth.Image",cloth.ClothesImage)
+            Column {
+                Text(text = "服のタイプ")
+                Text(text = cloth.ClothesType)
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(text = "服のカラー")
+                Text(text = cloth.ClothesColor)
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(text = "シーズン")
+                Text(text = cloth.ClothesScene)
+
+            }
         }
     }
 }
@@ -87,3 +91,12 @@ fun ClothsRowTest(){
         ),
         onClickRow = {}, onClickDelete = {})
 }
+
+/*
+IconButton(
+onClick = { onClickDelete(cloth)
+})
+{
+    Icon(imageVector = Icons.Default.Delete, contentDescription = "DeleteButton")
+    GlideImage(model = Uri.parse(cloth.ClothesImage), contentDescription = "服の写真")
+}*/
