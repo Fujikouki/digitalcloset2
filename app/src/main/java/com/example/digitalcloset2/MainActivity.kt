2a09d9d2347dc83442257a1df0f77767e1e6973a
Fujikouki <c1202599@st.kanazawa-it.ac.jp>
2024-04-21 12:34:53 +0900

    Themeを変更

--- app/src/main/java/com/example/digitalcloset2/MainActivity.kt
+++ app/src/main/java/com/example/digitalcloset2/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -23,24 +22,20 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.digitalcloset2.Camera.CameraManager
+import com.example.compose.AppTheme
 import com.example.digitalcloset2.components.ClothsList
 import com.example.digitalcloset2.components.Dialog
 import com.example.digitalcloset2.components.PermissionTest
-import com.example.digitalcloset2.ui.theme.DigitalCloset2Theme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val camera = CameraManager()
         setContent {
-            DigitalCloset2Theme {
+            AppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
 
                     HostScreen()
